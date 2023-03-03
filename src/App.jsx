0122bdf8493ce4a5d6b294be9897372fe36bc532
mazcoding <115239190+mazcoding@users.noreply.github.com>
2023-03-03 18:03:34 +0100

    update for pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Home />} />
             <Route path="/work" element={<Work />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
