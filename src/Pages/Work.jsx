febb70ad0ad8cac3a9550a1f1663f5c9d08da29b
mazcoding <115239190+mazcoding@users.noreply.github.com>
2023-03-13 09:13:52 +0100

    update

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -5,7 +5,6 @@ import GlobalSidebar from '../Elements/GlobalSidebar'
 import GlobalHeader from '../Elements/GlobalHeader'
 import Ghostbar from '../Elements/Ghostbar'
 import Footbar from '../Elements/Footbar'
-import { FaEnvelope, FaPhoneSquare } from 'react-icons/fa'
 import { ThemeContext } from '../Elements/AnimatedRoutes';
 
 function Work() {
@@ -27,24 +26,53 @@ return (
           alignItems="center"
           >
             {!isDesktop ? (
-            <Box position="sticky" w="100%"
+            <Box
+            position="sticky"
+            w="100%"
             top="0"
-            zIndex="sticky" >
+            zIndex="sticky">
             <GlobalHeader/>
             </Box>) : (<GlobalSidebar/>
             )}
-            <Box py="10" textColor={isClicked ? 'gray.400' : "white"} w="70vw" justify={"center"}>
-              <Stack justify="space-between" gap="2vw"  direction={{base:"column", md:"row"}}>
+            <Box
+            py="10"
+            textColor={isClicked ? 'gray.400' : "white"}
+            w="70vw"
+            justify={"center"}>
+              <Stack
+              justify="space-between"
+              gap="2vw"
+              direction={{base:"column", md:"row"}}>
                 <VStack gap="2vw">
                   <VStack>
-                    <Image boxShadow="dark-lg" transition='.5s ease-out' opacity={isClicked ? 0.8 : 1} borderRadius="1vh" src='TaraFrontTEXT.png' />
-                    <Button bg="transparent" border="1px" _hover={{bgColor:"#dbb2d7", borderColor:"transparent"}}fontFamily='Montserratbold'>MORE</Button>
+                    <Image
+                    boxShadow="dark-lg"
+                    transition='.5s ease-out'
+                    opacity={isClicked ? 0.8 : 1}
+                    borderRadius="1vh"
+                    src='TaraFrontTEXT.png'/>
+                    <Button
+                    bg="transparent"
+                    border="1px"
+                    _hover={{bgColor:"#dbb2d7", borderColor:"transparent"}}
+                    fontFamily='Montserratbold'
+                    >MORE</Button>
                   </VStack>
                 </VStack>
                 <VStack gap="2vw">
                   <VStack>
-                    <Image boxShadow="dark-lg" transition='.5s ease-out' opacity={isClicked ? 0.8 : 1} borderRadius="1vh" src='nbaFrontTEXT.png' />
-                    <Button bg="transparent" border="1px" _hover={{bgColor:"#badbce", borderColor:"transparent"}}fontFamily='Montserratbold'>MORE</Button>
+                    <Image
+                    boxShadow="dark-lg"
+                    transition='.5s ease-out'
+                    opacity={isClicked ? 0.8 : 1}
+                    borderRadius="1vh"
+                    src='nbaFrontText.png'/>
+                    <Button
+                    bg="transparent"
+                    border="1px"
+                    _hover={{bgColor:"#badbce", borderColor:"transparent"}}
+                    fontFamily='Montserratbold'
+                    >MORE</Button>
                   </VStack>
                 </VStack>
               </Stack>
